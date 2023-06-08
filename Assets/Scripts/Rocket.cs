ce082eebf663fc5e3fe6181fa08c784230bca1d5
Vedmich <78676683+RetroVedmich@users.noreply.github.com>
2023-06-08 11:35:10 +0500

    DoRocket
    Создали тестовую сцену, создали ракету, отрегулировали управление, назначили теги коллизии

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float rotateVelocity = 80.9f;
+
+    [SerializeField] private float flySpeed = 100f;
+
+    private Rigidbody _rigidBody;
+
+    private AudioSource _audioSource;
+
     void Start()
     {
-        
+        _rigidBody = GetComponent<Rigidbody>();
+        _audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        Launch();
+
+        Rotation();
+    }
+
+    private void Launch()
+    {
+        float launchSpeed = flySpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rigidBody.AddRelativeForce(Vector3.up * launchSpeed);
+
+            if (!_audioSource.isPlaying) _audioSource.Play();
+        }
+
+        else _audioSource.Pause();
+    }
+
+    private void Rotation()
+    {
+        float rotationSpeed = rotateVelocity * Time.deltaTime;
+
+        _rigidBody.freezeRotation = true;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+
+        _rigidBody.freezeRotation = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Battery":
+                Debug.Log("Вам добавили энергии");
+                Destroy(collision.gameObject);
+                break;
+            case "Finish":
+                Debug.Log("Вы победили!");
+                break;
+            default:
+                Debug.Log("Ракета взорвалась");
+                break;
+        }
     }
 }
